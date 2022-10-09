11b6b20ee83fa7885e54a9f9947e05c0798b6bdf
Yury Litvinov <litvinov.yury@gmail.com>
2022-10-08 21:07:52 -0500

    refactor

--- nodejs/src/chain-utils.ts
+++ nodejs/src/chain-utils.ts
@@ -17,18 +17,3 @@ export function getMaxBaseFeeInFuture2(baseFee: BigNumber, blocksInFuture: numbe
     }
     return maxBaseFee
 }
-
-export async function forwardRequestToProvider<P, ResBody, ReqBody, ReqQuery, Locals>(providerUrl: string, req) {
-    console.log('PROVIDER request: ', JSON.stringify(req.body));
-    let providerResponse = await axios.post(providerUrl, req.body, {
-            headers: {'Content-Type': 'application/json'}
-        }
-    );
-    console.log('PROVIDER response: ', JSON.stringify(providerResponse.data));
-    return providerResponse;
-}
-
-export function resSend<ResBody, Locals>(res: Response<ResBody, Locals>, data) {
-    console.log('Response: ', JSON.stringify(data));
-    res.send(data);
-}
