d0029a2f9bdaccbdaa53c8a6262bf5c9e2481268
Yury Litvinov <litvinov.yury@gmail.com>
2022-10-09 06:55:15 -0500

    rollback Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {userAddressAtom} from "./state";
 import Button from 'react-bootstrap/Button';
 import {signout} from "./actions";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function LogoutButton(props) {
     const [userAddress] = useRecoilState(userAddressAtom);
@@ -32,6 +33,12 @@ function App() {
     return (
         <RecoilRoot>
             <RecoilNexus/>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<h1>Home Page</h1>}/>
+                    <Route path="add-funds-success" element={<Page1/>}/>
+                </Routes>
+            </Router>
             <Navbar bg="light" expand="lg">
                 <Container>
                     <Navbar.Brand><img className="logo" src={"/GoGasCover.png"}></img></Navbar.Brand>
@@ -49,6 +56,14 @@ function App() {
             </div>
         </RecoilRoot>
     );
+}
+
+function Page1() {
+    return (
+        <div>
+            <h1>Add Funds Success</h1>
+        </div>
+    )
 }
 
 export default App;
